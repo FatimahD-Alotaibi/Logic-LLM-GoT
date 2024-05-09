46f00e5b5aef8207814cb050acf00d839b8d81c5
Jean-Paul <jpzambrano.leon@gmail.com>
2024-05-09 02:02:17 -0500

    created a helper function for scoring the response

--- logic_llm_got/folio_got/utils.py
+++ logic_llm_got/folio_got/utils.py
@@ -1,3 +1,25 @@
+from typing import Dict
+
+def answer_score(state: Dict) -> float:
+    """
+    Funtion to assign a float that serves as a score.
+
+    :param state: Tought state to be scored.
+    :type state: Dict
+    :return: Score
+    :rtype: float
+    """
+    try: 
+        predicted_answer = state["current"] # The answer given by the LLM
+        correct_answer = state["ground_truth"] # the actual answer
+        if predicted_answer == correct_answer:
+            return float(1.0) # Like most multiple choice problems, the grade point for answering correctly is 1
+        elif predicted_answer != correct_answer:
+            return float(0.0)
+    except:
+        return float(0.0)
+
+
 def test_response(state: str) -> bool:
     """
     Function to test whether the final solution matches ground truth.
@@ -9,8 +31,8 @@ def test_response(state: str) -> bool:
     """
 
     try:
-        ground_truth = state["ground_truth"]
-        predicted_response = state["current"]
+        ground_truth = state["ground_truth"] # The actual answer
+        predicted_response = state["current"] # The answer given by the LLM
         # Check if values are the same
         if ground_truth != predicted_response:
             return False
