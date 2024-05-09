aaac72613b37c61cec765185d6a881ff3bdcc7ff
Jean-Paul <jpzambrano.leon@gmail.com>
2024-05-09 01:58:55 -0500

    added documentation to logic_parser.py

---
+++ logic_llm_got/folio_got/logic_parser.py
@@ -0,0 +1,102 @@
+from typing import Dict, List, Union
+from graph_of_thoughts import parser
+import re
+import logging
+
+class SymbolicReasoningParser(parser.Parser):
+    """
+    SymbolicCountingParser provides the parsing of language model reponses
+    specific to the keyword counting example.
+
+    Inherits from the Parser class and implements its abstract methods.
+    """
+
+    def __init__(self) -> None:
+        """
+        Inits the response cache.
+        """
+        self.cache = {}
+
+    def extract_only_letter_character(self, text: str) -> str:
+        """
+        
+        """
+        match = re.search(r'[A-C]', text)
+        if match:
+            return match.group()
+        else:
+            return None
+    
+    def parse_aggregation_answer(self, states: List[Dict], texts: List[str]) -> Union[Dict, List[Dict]]:
+        """
+        Parse the response from the language model for an aggregation prompt.
+
+        :param states: The thought states used to generate the prompt.
+        :type states: List[Dict]
+        :param texts: The responses to the prompt from the language model.
+        :type texts: List[str]
+        :return: The new thought states after parsing the respones from the language model.
+        :rtype: Union[Dict, List[Dict]]
+        :raise AssertionError: If more than two thought states are provided.
+        """
+        pass
+
+    def parse_improve_answer(self, state: Dict, texts: List[str]) -> Dict:
+        """
+        Parse the response from the language model for an improve prompt.
+
+        :param state: The thought state used to generate the prompt.
+        :type state: str
+        :param texts: The responses to the prompt from the language model.
+        :type texts: List[str]
+        :return: The new thought state after parsing the responses from the language model.
+        :rtype: str
+        :raise AssertionError: If there is not exactly one response text.
+        """
+        pass
+
+    def parse_generate_answer(self, state: Dict, texts: List[str]) -> List[Dict]:
+        """
+        Parse the response from the language model for a generate prompt.
+
+        :param state: The thought state used to generate the prompt.
+        :type state: Dict
+        :param texts: The responses to the prompt from the language model.
+        :type texts: List[str]
+        :return: The new thought states after parsing the respones from the language model.
+        :rtype: List[Dict]
+        """
+        new_states = []
+        for text in texts:
+            answer = self.extract_only_letter_character(text)
+            new_state = state.copy()
+            new_state["current"] = answer
+            new_states.append(new_state)
+        return new_states
+
+    def parse_validation_answer(self, state: Dict, texts: List[str]) -> bool:
+        """
+        Parse the response from the language model for a validation prompt.
+
+        :param state: The thought state used to generate the prompt.
+        :type state: Dict
+        :param texts: The responses to the prompt from the language model.
+        :type texts: List[str]
+        :return: Whether the thought state is valid or not.
+        :rtype: bool
+        """
+        pass
+
+    def parse_score_answer(self, states: List[Dict], texts: List[str]) -> List[float]:
+        """
+        Parse the response from the language model for a score prompt.
+
+        :param states: The thought states used to generate the prompt.
+        :type states: List[Dict]
+        :param texts: The responses to the prompt from the language model.
+        :type texts: List[str]
+        :return: The scores for the thought states.
+        :rtype: List[float]
+        :raise AsssertionError: If the number of thought states is not one.
+        """
+        pass
